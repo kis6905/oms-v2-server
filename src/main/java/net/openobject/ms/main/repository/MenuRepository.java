87e62256dd11bf2894f73e898a2db32258f41430
kis6905 <kis6905@gmail.com>
2019-01-12 15:21:11 +0900

    Update: 정리

--- src/main/java/net/openobject/ms/main/repository/MenuRepository.java
+++ src/main/java/net/openobject/ms/main/repository/MenuRepository.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import net.openobject.ms.main.dto.Menu;
-import net.openobject.ms.user.dto.User;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-	
-	User findByUserId(String userId);
 	
 }
